75d734ade8fc9be7c75ae8314bace5253d029aba
m-grgic <matija.grgicc@gmail.com>
2022-09-17 16:42:37 +0200

    m

--- backend/src/api-calls/api-calls.controller.ts
+++ backend/src/api-calls/api-calls.controller.ts
@@ -1,28 +1,27 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { ApiParam } from '@nestjs/swagger';
 import { ApiCallsService } from './api-calls.service';
 import { OtherData, PriceData, VolumeData } from './interfaces';
 
 @Controller('api-calls')
 export class ApiCallsController {
-    constructor(private apiService: ApiCallsService) {}
-    
+    constructor(private apiService: ApiCallsService) { }
+
 
     @Get("getPriceData/:name/:days")
-    async getPriceData(@Param("name") name:string, @Param("days") days:number): Promise<PriceData>{
-        const response= await this.apiService.getPriceData(name,days);
+    async getPriceData(@Param("name") name: string, @Param("days") days: number): Promise<PriceData> {
+        const response = await this.apiService.getPriceData(name, days);
         return response;
-     }
+    }
 
-     @Get("getVolumeData")
-     async getVolumeData(@Param("name") name:string,@Param("days") days:number):Promise<VolumeData>{
-        const response=await this.apiService.getVolumeData(name,days);
+    @Get("getVolumeData")
+    async getVolumeData(@Param("name") name: string, @Param("days") days: number): Promise<VolumeData> {
+        const response = await this.apiService.getVolumeData(name, days);
         return response
-     }
+    }
 
-     @Get("getOtherData")
-     async getOtherData(@Param("name") name:string):Promise<OtherData>{
-        const response=await this.apiService.getOtherData(name);
+    @Get("getOtherData")
+    async getOtherData(@Param("name") name: string): Promise<OtherData> {
+        const response = await this.apiService.getOtherData(name);
         return response;
-     }
+    }
 }
--- backend/src/api-calls/api-calls.service.ts
+++ backend/src/api-calls/api-calls.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ScriptableLineSegmentContext } from 'chart.js';
 import { OtherData, PriceData, VolumeData } from './interfaces';
+import axios from 'axios';
 
- 
+
 function timeConverter(UNIX_timestamp) {
     let a = new Date(UNIX_timestamp);
     let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
@@ -11,55 +11,55 @@ function timeConverter(UNIX_timestamp) {
     let date = a.getDate();
     let time = date + '/' + month + '/' + year;
     return time;
-  }
+}
 
 
 @Injectable()
 export class ApiCallsService {
 
 
-    async getPriceData(name: string, days: number) : Promise<PriceData>{
-    const dataPromise = await fetch("https://api.coingecko.com/api/v3/coins/" + name + "/market_chart?vs_currency=usd&days=" + days + "&interval=daily");
-    const market = await dataPromise.json();
-    let temp= market.prices;
-    let prices=[];
-    let dates=[];
-       temp.forEach(element => {
-         prices.push(element[1]);
-         dates.push(timeConverter(element[0]));
+    async getPriceData(name: string, days: number): Promise<PriceData> {
+        const dataPromise = await axios.get("https://api.coingecko.com/api/v3/coins/" + name + "/market_chart?vs_currency=usd&days=" + days + "&interval=daily");
+        const market = await dataPromise.data;
+        let temp = market.prices;
+        let prices = [];
+        let dates = [];
+        temp.forEach(element => {
+            prices.push(element[1]);
+            dates.push(timeConverter(element[0]));
         });
-    let data={name,prices,dates};
-    return data;
+        let data = { name, prices, dates };
+        return data;
     }
 
-     async getVolumeData(name:string, days:number):Promise<VolumeData> {
-        const dataPromise = await fetch("https://api.coingecko.com/api/v3/coins/" + name + "/market_chart?vs_currency=usd&days=" + days + "&interval=daily");
-        const market = await dataPromise.json();
-        let temp= market.total_volumes;
-        let volumes=[];
-        let dates=[];
-           temp.forEach(element => {
-             volumes.push(element[1]);
-             dates.push(timeConverter(element[0]));
-            });
-        return {name,volumes,dates};
-        
-      }
+    async getVolumeData(name: string, days: number): Promise<VolumeData> {
+        const dataPromise = await axios.get("https://api.coingecko.com/api/v3/coins/" + name + "/market_chart?vs_currency=usd&days=" + days + "&interval=daily");
+        const market = await dataPromise.data;
+        let temp = market.total_volumes;
+        let volumes = [];
+        let dates = [];
+        temp.forEach(element => {
+            volumes.push(element[1]);
+            dates.push(timeConverter(element[0]));
+        });
+        return { name, volumes, dates };
 
-       async getOtherData(name:string):Promise<OtherData>{
-        const dataPromise=await fetch("https://api.coingecko.com/api/v3/coins/"+name+"?localization=false&market_data=true");
-        const obj=await dataPromise.json();
-        let id= obj.id;
-        let symbol= obj.symbol;
-        let symbName=obj.name;
-        let hash=obj.hashing_algorithm;
-        let desc=obj.description.en;
-        let rank=obj.market_cap_rank;
-        let price=obj.market_data.current_price.usd;
-        let ath=obj.market_data.ath.usd;
-        let cap= obj.market_data.market_cap.usd;
-        let volume=obj.market_data.total_volume.usd;
-        return {id,symbol,symbName,hash,desc,rank,price,ath,cap,volume};
-        
-      }
+    }
+
+    async getOtherData(name: string): Promise<OtherData> {
+        const dataPromise = await axios.get("https://api.coingecko.com/api/v3/coins/" + name + "?localization=false&market_data=true");
+        const obj = await dataPromise.data;
+        let id = obj.id;
+        let symbol = obj.symbol;
+        let symbName = obj.name;
+        let hash = obj.hashing_algorithm;
+        let desc = obj.description.en;
+        let rank = obj.market_cap_rank;
+        let price = obj.market_data.current_price.usd;
+        let ath = obj.market_data.ath.usd;
+        let cap = obj.market_data.market_cap.usd;
+        let volume = obj.market_data.total_volume.usd;
+        return { id, symbol, symbName, hash, desc, rank, price, ath, cap, volume };
+
+    }
 }
